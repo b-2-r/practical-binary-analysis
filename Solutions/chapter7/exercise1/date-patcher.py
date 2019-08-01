b711ab578db2b32aeb11887d7429ed486910d6ee
Manuel Gebele <m.gebele@tuta.io>
2019-08-01 22:27:19 +0200

    Renamed date-patch.py to date-patcher.py

--- Solutions/chapter7/exercise1/date-patcher.py
+++ Solutions/chapter7/exercise1/date-patcher.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-# date-patch.py
+# date-patcher.py
 #
 # Written by Manuel Gebele (m.gebele‹ατ›tuta.io)
 # This code is licensed under the MIT License (MIT). 

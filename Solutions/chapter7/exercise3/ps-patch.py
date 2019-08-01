76df10d4c9956df9e15bcb4f28bfd693e57c608b
Manuel Gebele <m.gebele@tuta.io>
2019-08-01 22:04:03 +0200

    Fixed copy/paste typos

--- Solutions/chapter7/exercise3/ps-patch.py
+++ Solutions/chapter7/exercise3/ps-patch.py
@@ -21,30 +21,30 @@ READPROCTAB2_PATCH = (
 
 def ps_path():
     which_output = subprocess.check_output(['which', 'ps'])
-    date_path = which_output.decode('utf-8').strip()
-    return date_path
+    ps_path = which_output.decode('utf-8').strip()
+    return ps_path
 
 def ps_mod_path():
     file_abs_path = os.path.abspath(__file__)
     file_dir_path = os.path.dirname(file_abs_path)
-    date_mod_path = file_dir_path + "/ps-modified"
-    return date_mod_path
+    ps_mod_path = file_dir_path + "/ps-modified"
+    return ps_mod_path
 
 def make_ps_copy():
-    dp = ps_path()
-    dmp = ps_mod_path()
-    print("[*] Copying " + dp + " to " + dmp)
-    shutil.copy(dp, dmp)
+    pp = ps_path()
+    pmp = ps_mod_path()
+    print("[*] Copying " + pp + " to " + pmp)
+    shutil.copy(pp, pmp)
 
-def apply_patch(ps_bytes, patch):
+def apply_patch(bin_bytes, patch):
     orig_bytes, patched_bytes = patch
-    idx = ps_bytes.find(orig_bytes)
+    idx = bin_bytes.find(orig_bytes)
     if -1 == idx:
         print("[*] Couldn't find " + orig_bytes.hex() + " byte sequence")
         exit(1)
     print("[*] Applying patch " + patched_bytes.hex() + " at offset " + str(hex(idx)))
-    ps_bytes = ps_bytes.replace(orig_bytes, patched_bytes)
-    return ps_bytes
+    bin_bytes = bin_bytes.replace(orig_bytes, patched_bytes)
+    return bin_bytes
 
 def apply_patches():
     make_ps_copy()
@@ -52,14 +52,14 @@ def apply_patches():
     pmp = ps_mod_path()
     ps_file = open(pmp, 'r+b')
 
-    ps_bytes = ps_file.read()
+    bin_bytes = ps_file.read()
     ps_file.seek(0)
 
-    ps_bytes = apply_patch(ps_bytes, READPROC_PATCH)
-    ps_bytes = apply_patch(ps_bytes, READPROCTAB2_PATCH)
+    bin_bytes = apply_patch(bin_bytes, READPROC_PATCH)
+    bin_bytes = apply_patch(bin_bytes, READPROCTAB2_PATCH)
 
     # Write modifications to disk.
-    ps_file.write(ps_bytes)
+    ps_file.write(bin_bytes)
 
     ps_file.close()
 
